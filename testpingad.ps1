a1aea6db8b116c8b2d8c695767b0846e2f11abdb
Moe <57568144+MoeChad@users.noreply.github.com>
2020-06-16 21:03:58 -0400

    Update testpingad.ps1

--- testpingad.ps1
+++ testpingad.ps1
@@ -1,68 +1,30 @@
-function Get-PingAD {
-
-
 Add-Type -AssemblyName System.Windows.Forms
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop')
 Filter = 'Documents (*.txt)|*.txt' }
 
 $null = $FileBrowser.ShowDialog()
 
-<#get-content -Path $FileBrowser.FileName | ForEach-Object {
-    $test = Test-Connection -ComputerName $_ -Quiet -Count 1
-    if($test) {
-    New-Object -TypeName PSCustomObject -Property @{
-        MachineName = $_;
-        'Ping Status' = 'Success';
-    }
-}
-    else {
-    New-Object -TypeName PSCustomObject -Property @{
-        MachineName = $_;
-        'Ping Status' = 'Fail';
-        }
-    }
-} | Export-Csv C:\Powershell\ping.csv #>
-
 $computer = get-content -path $filebrowser.FileName
 
 
-$output = $computer | ForEach-Object {
-    $computer = $_
-    try {
-        $adcomp = Get-ADComputer -Identity $computer -ErrorAction Stop | Select-Object -ExpandProperty Enabled
-        if($adcomp) {
-            $test = Test-Connection -ComputerName $computer -quiet -count 1
-            if($test) {
-                [PSCustomObject]@{
-                    MachineName = $computer
-                    PingTest    = $True
-                }
-            Write-Verbose ('{0} is On' -f $computer)
-            }
-            else {
-                [PSCustomObject]@{
-                    MachineName = $computer
-                    PingTest    = $false
-                }
-            Write-Verbose ('{0} is Off' -f $computer)
-            }
-        }
-        else {
-            [PSCustomObject]@{
-                MachineName = $computer
-                PingTest    = $false
-            }
-        Write-Verbose ('{0} is not in AD' -f $computer)
-        }
-    }
+$Results = foreach ($machine in $computer) {
+    Try {
+    $adcomp = Get-ADComputer -Identity $machine -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Enabled
 
-    catch {
-        [PSCustomObject]@{
-            MachineName = $computer
-            PingTest    = $false
+    [PSCustomObject]@{
+        MachineName = $machine
+        AD          = $adcomp
+        PingTest    = $adcomp -and (Test-Connection -Computername $machine -quiet -count 1)
         }
-        Write-Verbose ('{0} is not in AD' -f $computer)
+    }
+    catch{
+            [PSCustomObject]@{
+        MachineName = $machine
+        AD          = $false
+        PingTest    = $false
+        }
     }
 }
-    $output | export-csv -path C:\Powershell\pingAD.csv
-}
+
+
+$Results | Export-Csv -Path C:\Powershell\PingAD.csv
